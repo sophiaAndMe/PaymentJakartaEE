9b823f235534c101ab4b02bb8a7e7cb445a73f23
sophiaAndMe <jjlaverde@uce.edu.ec>
2024-12-03 20:09:14 -0500

    ADD APIRest DINAMI :) Problems? method delete

--- src/main/java/ec/edu/uce/laverdejhon_paymentjakarta/application/HelloResource.java
+++ src/main/java/ec/edu/uce/laverdejhon_paymentjakarta/application/HelloResource.java
@@ -1,6 +1,6 @@
 package ec.edu.uce.laverdejhon_paymentjakarta.application;
 
-import ec.edu.uce.laverdejhon_paymentjakarta.Payment.CardPayment;
+
 import ec.edu.uce.laverdejhon_paymentjakarta.entity.Customer;
 import ec.edu.uce.laverdejhon_paymentjakarta.entity.CustomerService;
 import jakarta.inject.Inject;
@@ -8,9 +8,9 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 import jakarta.transaction.Transactional;
-import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 
 @Path("/test")
@@ -18,26 +18,44 @@ public class HelloResource {
 
 
 
-    @Path("/create")
+    //--> Delete customer
+    //--> ARREGLAR EL DELETE :)
+    @Path("/delete={id}")
     @GET
     @Produces("text/plain")
     @Transactional
-    public String createCustomer() {
+    public String deleteCustomer(@PathParam("id")int id){
+
+        CustomerService cs = new CustomerService();
+        cs.delete(id);
+        return "Se ha elimiando el customer"+ id;
+    }
+
+    //--> Dinamismo con el APIRest
+
+
+    @Path("/id={id}/name={name}/address={address}")
+    @GET
+    @Produces("text/plain")
+    @Transactional
+    public String createCustomerV2(@PathParam("id")long id, @PathParam("name")String name,
+                                        @PathParam("address") String address){
+
         EntityManagerFactory emp = Persistence.createEntityManagerFactory("EntityP");
         EntityManager em = emp.createEntityManager();
         CustomerService cs = new CustomerService(em);
-        cs.create(new Customer(1,"Jhon","@example.com",
-                "PaypalPayment","SuperMaxi","500"));
 
+        if (id ==0 || name == null){
+            return "Te olvidaste de poner el id y el nombre :))";
+
+        }else {
+            cs.create(new Customer(id, name,address, "paypalpayment",
+                    "super", "1000"));
+        }
         return cs.toString();
     }
 
-    @Path("/pay")
-    @GET
-    @Produces("text/plain")
-    public String payment() {
-        return null;
-    }
+
 
 
 }
