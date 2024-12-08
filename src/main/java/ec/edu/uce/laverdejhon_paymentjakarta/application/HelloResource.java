5eedc0a33e65d91038122d1daa18b3644e5eb8e8
sophiaAndMe <jjlaverde@uce.edu.ec>
2024-12-08 16:39:01 -0500

    test @OneToMany

--- src/main/java/ec/edu/uce/laverdejhon_paymentjakarta/application/HelloResource.java
+++ src/main/java/ec/edu/uce/laverdejhon_paymentjakarta/application/HelloResource.java
@@ -3,10 +3,13 @@ package ec.edu.uce.laverdejhon_paymentjakarta.application;
 
 import ec.edu.uce.laverdejhon_paymentjakarta.entity.Customer;
 import ec.edu.uce.laverdejhon_paymentjakarta.entity.CustomerService;
+import ec.edu.uce.laverdejhon_paymentjakarta.entity.OrderPayment;
+import ec.edu.uce.laverdejhon_paymentjakarta.entity.OrderPaymentService;
 import jakarta.inject.Inject;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+import jakarta.persistence.PersistenceContext;
 import jakarta.transaction.Transactional;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
@@ -17,42 +20,56 @@ import jakarta.ws.rs.Produces;
 public class HelloResource {
 
 
+    @Inject
+    CustomerService customerService;
 
     //--> Delete customer
     //--> ARREGLAR EL DELETE :)
-    @Path("/delete={id}")
     @GET
+    @Path("/delete={id}")
     @Produces("text/plain")
     @Transactional
-    public String deleteCustomer(@PathParam("id")int id){
-
-        CustomerService cs = new CustomerService();
-        cs.delete(id);
+    public String deleteCustomer(@PathParam("id")long id){
+        customerService.delete(id);
         return "Se ha elimiando el customer"+ id;
     }
 
     //--> Dinamismo con el APIRest
 
 
-    @Path("/id={id}/name={name}/address={address}")
+    @Path("/name={name}/address={address}")
     @GET
     @Produces("text/plain")
     @Transactional
-    public String createCustomerV2(@PathParam("id")long id, @PathParam("name")String name,
+    public String createCustomerV2(@PathParam("name")String name,
                                         @PathParam("address") String address){
 
         EntityManagerFactory emp = Persistence.createEntityManagerFactory("EntityP");
         EntityManager em = emp.createEntityManager();
         CustomerService cs = new CustomerService(em);
 
-        if (id ==0 || name == null){
-            return "Te olvidaste de poner el id y el nombre :))";
+        if (name == null){
+            return "INGRESA UN NOMBRE ))";
 
         }else {
-            cs.create(new Customer(id, name,address, "paypalpayment",
+            cs.create(new Customer(name,address, "paypalpayment",
                     "super", "1000"));
         }
         return cs.toString();
+    }
+
+
+    @Path("/order")
+    @GET
+    @Transactional
+    public String createOrder(){
+        EntityManagerFactory emp = Persistence.createEntityManagerFactory("EntityP");
+        EntityManager em = emp.createEntityManager();
+        OrderPaymentService orp = new OrderPaymentService(em);
+
+        orp.create(new OrderPayment(1));
+
+        return orp.toString();
     }
 
 
